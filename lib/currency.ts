9e55c2dbbd22e0d59db44ad343614ffb4fcab1dc
horlard <oladipuposaulih@gmail.com>
2025-11-06 14:56:16 +0100

    Add formatCurrency function for formatting amounts in NGN currency

---
+++ lib/currency.ts
@@ -0,0 +1,7 @@
+export const formatCurrency = (amount: number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "NGN",
+    minimumFractionDigits: 2,
+  }).format(amount);
+};
