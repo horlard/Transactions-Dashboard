124614b3a8cb029c648392d380ae1d3f9cd96408
horlard <oladipuposaulih@gmail.com>
2025-11-07 14:46:22 +0100

    Refactor Transaction type definition to improve clarity and structure

--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,11 @@
-export interface Transaction {
+export interface IObject {
+  [key: string]: string | number | boolean | null | undefined | unknown;
+}
+
+export type Transaction = {
   id: string;
   description: string;
   amount: number;
   type: "credit" | "debit";
   date: string;
-}
+};
